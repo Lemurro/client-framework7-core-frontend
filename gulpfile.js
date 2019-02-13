ba52956e1fd0c1e6e75eab038dfbe7e2d26e8e94
Дмитрий Щербаков <atomcms@ya.ru>
2019-02-13 15:17:11 +0500

    feat Переход на Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,49 @@ var concat   = require('gulp-concat');    // Склейка файлов
 var rename   = require('gulp-rename');    // Переименование файлов
 var uglify   = require('gulp-uglify');    // Минификация JS
 
-gulp.task('default', ['css', 'js'], function () {
-    gulp.watch('src/css/*.css', ['css']);
-    gulp.watch('src/js/**/*.js', ['js']);
-});
+// WATCHER
 
-gulp.task('build', ['css', 'js']);
+function watcherCSS() {
+    return gulp.watch('src/css/*.css', gulp.series(coreCSS, coreMinCSS));
+}
 
-gulp.task('css', function () {
-    gulp.src('src/css/*.css')
+function watcherJS() {
+    return gulp.watch('src/js/**/*.js', gulp.series(coreJS, coreMinJS));
+}
+
+// CSS
+function coreCSS() {
+    return gulp.src('src/css/*.css')
         .pipe(concat('core.css'))
         .pipe(gulp.dest('dist'));
+}
 
-    gulp.src('src/css/*.css')
-        .pipe(concat('core.css'))
+function coreMinCSS() {
+    return gulp.src('dist/core.css')
         .pipe(cleanCSS())
         .pipe(rename('core.min.css'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('js', function () {
-    gulp.src('src/js/**/*.js')
+// JS
+function coreJS() {
+    return gulp.src('src/js/**/*.js')
         .pipe(concat('core.js'))
         .pipe(gulp.dest('dist'));
+}
 
-    gulp.src('src/js/**/*.js')
-        .pipe(concat('core.js'))
+function coreMinJS() {
+    return gulp.src('dist/core.js')
         .pipe(uglify())
         .pipe(rename('core.min.js'))
         .pipe(gulp.dest('dist'));
-});
+}
+
+// TASKS
+
+gulp.task('build', gulp.parallel(
+    gulp.series(coreCSS, coreMinCSS),
+    gulp.series(coreJS, coreMinJS)
+));
+
+gulp.task('watcher', gulp.series('build', gulp.parallel(watcherCSS, watcherJS)));
